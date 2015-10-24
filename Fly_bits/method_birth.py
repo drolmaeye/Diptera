8052cdd2e4070fe6644b1c2d0bbe3a30ffca3885
Jesse Smith <jssmith@ciw.edu>
2015-10-24 14:35:39 -0500

    create instance inside a function

---
+++ Fly_bits/method_birth.py
@@ -0,0 +1,27 @@
+__author__ = 'j.smith'
+
+from epics import *
+from Tkinter import *
+
+
+
+def initialize():
+    global m1
+    m1 = Motor('16TEST1:m1')
+    return m1
+
+
+def rbv():
+    print m1.get('RBV')
+
+
+root = Tk()
+button = Button(root, text='initialize', command=initialize)
+button.pack()
+button2 = Button(root, text='get RBV', command=rbv)
+button2.pack()
+root.mainloop()
+
+
+
+
